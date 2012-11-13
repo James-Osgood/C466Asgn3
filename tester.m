e8e29f112dbcac737d567a5c1dca4663f18bb977
James-Osgood <jamesosogood@gmail.com>
2012-11-13 15:11:40 -0700

    Added some time keeping.

--- tester.m
+++ tester.m
@@ -2,13 +2,21 @@ function [results] = tester()
 % results is a bunch of results gathered from running various learners on the
 % data
 
+    now() % start
+
     load data3.mat;
     [t, n] = size(X);
     
+    now() % before learn
+    
     model = learn(X, y);
+    
+    now() % after learn
     
     yhat = classify(X, model);
     
     results = sum(yhat == y) / t;
+    
+    now() % finished
 
 end
